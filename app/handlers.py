10ed8b5372a6083510954974edde95161b1b5ec3
dashkared <79451119+dashkared@users.noreply.github.com>
2025-04-15 20:23:04 +0300

    Update handlers.py

--- app/handlers.py
+++ app/handlers.py
@@ -67,3 +67,7 @@ async def task(callback: CallbackQuery):
 async def return_back(callback: CallbackQuery):
     await callback.message.edit_text('Выберите пункт меню', reply_markup=kb.inline_main)
 
+@router.callback_query(F.data == 'back')
+async def return_back(callback: CallbackQuery):
+    await callback.message.edit_text('Выберите пункт меню', reply_markup=kb.inline_main)
+
