56416bd44c5c36eb5e9f7aa5e7590c5aaec29948
dashkared <shkaredny2005@mail.ru>
2025-04-15 20:31:59 +0300

    1.2

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import asyncio
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+from app.handlers import router
+from app.database.models import async_main
+
+
+
+
+
+
+
+
+# Функция запуска бота
+async def main():
+    load_dotenv()
+    await async_main()
+    print("Бот запущен!")
+    bot = Bot(token=os.getenv('TG_TOKEN'))
+    dp = Dispatcher()
+    dp.include_router(router)
+    # Запускаем диспетчер
+    await dp.start_polling(bot)
+
+# Запуск бота с обработкой ошибок
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print("Бот остановлен.")
+
