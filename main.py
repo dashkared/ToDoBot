57d37db25acd2000ef259695dc602005a9668af1
dashkared <shkaredny2005@mail.ru>
2025-05-18 12:31:00 +0300

    fixed bugs

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 import asyncio
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
+from app.database.models import async_main
 
 async def main():
     load_dotenv()
-
+    await async_main()
     bot = Bot(token=os.getenv('TG_TOKEN'))
     dp = Dispatcher()
     print('Бот запущен')
